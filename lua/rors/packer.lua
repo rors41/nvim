a28f08f3d48506400b5847d955850ad9da702855
Matej Martinkovič <32226323+rors41@users.noreply.github.com>
2023-11-13 13:12:32 +0100

    Removed copilot

--- lua/rors/packer.lua
+++ lua/rors/packer.lua
@@ -71,7 +71,6 @@ return require('packer').startup(function(use)
     }
 
     use("folke/zen-mode.nvim")
-    use("github/copilot.vim")
     use("eandrju/cellular-automaton.nvim")
     use("folke/tokyonight.nvim")
 
